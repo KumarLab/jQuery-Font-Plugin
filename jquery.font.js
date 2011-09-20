7d58f2ab728ec2efec1ab19c3f02e076dd99aeb4
Visual Idiot <iam@visualidiot.com>
2011-09-20 10:14:30 +0100

    Standardised the text string to compare, reliability enhancements.

--- jquery.font.js
+++ jquery.font.js
@@ -9,27 +9,31 @@
 
 $.fn.font = function() {
 
-	var me = $(this),
-	stack = (me.css('font-family') ? me.css('font-family').split(', ') : [undefined]),
-		i = 0,
-		current = '',
-		body = $('body'),
-		style = 'position: absolute; padding: 0; width: auto; font-size: 40px; left: -999em; top: -999em;',
-		comicStack = body.append('<div id="comic-stack" style="' + style + ' font-family: \'Comic Sans MS\', \'Comic Sans\'">My word! This quail WAS DEVOUT? 107 Times.</div>').find('#comic-stack');
+	var stack = ($(this).css('font-family') ? $(this).css('font-family').split(', ') : [undefined]), //  Get the list of font families, or an empty array.
+		current = '', //  Set a pointer to report the current font. Since we don't know what it is, let's make it empty.
+		body = $('body'), //  Store the jQuery body object, since we'll use it more than once.
+		style = 'position: absolute; padding: 0; width: auto; font-size: 40px; left: -999em; top: -999em;', //  Same thing for the style stack. Easier to change.
+		text = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz 0123456789 ,./;][=-~`', //  Get as many characters tested as possible.
+		comicStack = body.append('<div id="comic-stack" style="' + style + ' font-family: \'Comic Sans MS\', \'Comic Sans\'">' + text + '</div>').find('#comic-stack'); // Standardised the text string to compare, reliability enhancements.
 	
 	if(stack[0] != 'Comic Sans MS') {
 		for(i = 0; i <= stack.length; i++) {
 			id = new Date().getTime();
-			family = body.append('<div id="stack-' + id + '" style="' + style + ' font-family: ' + stack[i] + ', \'Comic Sans MS\', \'Comic Sans\'">The quick brown fox jumps over the lazy dog. MmWwMwWm</div>')
+			family = body.append('<div id="stack-' + id + '" style="' + style + ' font-family: ' + stack[i] + ', \'Comic Sans MS\', \'Comic Sans\'">' + text + '</div>')
 						 .find('#stack-' + id);
 			width = family.css('width');
 			
 			family.remove();
 
+			//	If we have a match
 			if(width != comicStack.width()) {
+				//	Set it as the current font to report
 				current = stack[i];
 				
+				//	Hide the original testbed
 				comicStack.remove();
+				
+				//	Stop going through the other fonts
 				break;
 			}
 		}
